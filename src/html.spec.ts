cd3f4c9dd3378712883cdfcd6a4f13b757bda2cf
Zhicheng WANG <zcwang@thoughtworks.com>
2018-12-30 20:43:12 +0800

    Initial commit

---
+++ src/html.spec.ts
@@ -0,0 +1,55 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import { markAndSwapAll, restructureTable } from './html';
+import { JSDOM } from 'jsdom';
+
+describe('html', function () {
+  it('should mark and restructure table', () => {
+    const dom = new JSDOM(`<table>
+<thead>
+<tr>
+  <th>one</th>
+  <th>two</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+  <td>一</td>
+  <td>二</td>
+</tr>
+<tr>
+  <td>three</td>
+  <td>four</td>
+</tr>
+<tr>
+  <td>三</td>
+  <td>四</td>
+</tr>
+</tbody>
+</table>`);
+    const body = dom.window.document.body;
+    restructureTable(body);
+    expect(body.innerHTML).eql(`<table>
+<thead>
+<tr>
+  <th><p>one</p><p>一</p></th>
+  <th><p>two</p><p>二</p></th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+  <td><p>three</p><p>三</p></td>
+  <td><p>four</p><p>四</p></td>
+</tr>
+
+</tbody>
+</table>`);
+  });
+  it('should mark and swap translation and origin', () => {
+    const dom = new JSDOM(`<p>a</p><p>one</p><p>一</p><script>const a = 1;</script>`);
+    const body = dom.window.document.body;
+    markAndSwapAll(body);
+    expect(body.innerHTML).eql(`<p>a</p><p translation-result="on">一</p><p translation-origin="off">one</p><script>const a = 1;</script>`);
+  });
+});
---
+++ src/html.ts
@@ -0,0 +1,82 @@
+export function markAndSwapAll(body: HTMLElement): void {
+  restructureTable(body);
+  mark(body, 'p,h1,h2,h3,h4,h5,h6,header');
+  mark(body, 't,span,a');
+  swap(body);
+}
+
+export function mark(node: ParentNode, selector: string): void {
+  const elements = node.querySelectorAll(selector);
+  elements.forEach(node => {
+    if (containsChinese(node.textContent!)) {
+      const prev = node.previousElementSibling;
+      if (prev && prev.tagName === node.tagName && !containsChinese(prev.textContent!)) {
+        node.setAttribute('translation-result', 'on');
+        prev.setAttribute('translation-origin', 'off');
+        // 交换 id，中文内容应该占用原文的 id
+        const id = prev.getAttribute('id');
+        if (id) {
+          prev.removeAttribute('id');
+          node.setAttribute('id', id);
+        }
+        const href = prev.getAttribute('href');
+        if (href) {
+          node.setAttribute('href', href);
+        }
+      }
+    }
+  });
+}
+
+// 重塑表格结构
+export function restructureTable(node: ParentNode): void {
+  const items = node.querySelectorAll('table');
+  items.forEach(table => {
+    // 对出现在 thead 的行和出现在 tbody 的行进行统一处理
+    const rows = table.querySelectorAll('* > tr');
+    const translationRows: HTMLElement[] = [];
+    for (let i = 0; i < rows.length - 1; ++i) {
+      const thisRow = rows.item(i) as HTMLTableRowElement;
+      const nextRow = rows.item(i + 1) as HTMLTableRowElement;
+      if (containsChinese(nextRow.textContent!!)) {
+        translationRows.push(nextRow);
+        mergeRows(thisRow, nextRow);
+      }
+    }
+    translationRows.forEach(row => row.remove());
+  });
+}
+
+function mergeRows(originRow: HTMLTableRowElement, translationRow: HTMLTableRowElement): void {
+  const document = originRow.ownerDocument!;
+  if (originRow.cells.length !== translationRow.cells.length) {
+    throw 'Origin row must have same cells count with translation row!';
+  }
+  for (let i = 0; i < originRow.cells.length; ++i) {
+    const originCell = originRow.cells.item(i)!;
+    const translationCell = translationRow.cells.item(i)!;
+    const originP = document.createElement('p');
+    for (let j = 0; j < originCell.childNodes.length; ++j) {
+      originP.append(originCell.childNodes.item(j)!);
+    }
+    const translationP = document.createElement('p');
+    for (let j = 0; j < translationCell.childNodes.length; ++j) {
+      translationP.append(translationCell.childNodes.item(j)!);
+    }
+    originCell.appendChild(originP);
+    originCell.appendChild(translationP);
+  }
+}
+
+export function swap(node: ParentNode): void {
+  const pairList = node.querySelectorAll('[translation-origin]+[translation-result]');
+  pairList.forEach(node => {
+    const prev = node.previousElementSibling;
+    node.parentElement!.insertBefore(node, prev);
+  });
+}
+
+function containsChinese(text: string): boolean {
+  return text.search(/[\u4e00-\u9fa5]/gm) !== -1;
+}
+
