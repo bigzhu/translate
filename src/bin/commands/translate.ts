31987a8724c09c6c245fa53aede5456e5872e798
Zhicheng WANG <zcwang@thoughtworks.com>
2019-02-22 21:55:49 +0800

    fix: 就地翻译，不再输出到其它目录
    在设计场景下，它通常会和版本管理工具配套使用

--- src/bin/commands/translate.ts
+++ src/bin/commands/translate.ts
@@ -3,16 +3,13 @@ import { TranslationKit } from '../../translation-kit';
 import { TranslationEngineType } from '../../common';
 import * as toVFile from 'to-vfile';
 
-export const command = `translate <sourceGlob> <outDir>`;
+export const command = `translate <sourceGlob>`;
 
 export const describe = '把 sourceGlob 中的内容自动翻译的 targetDir 中';
 
 export const builder: CommandBuilder = {
   sourceGlob: {
     description: '文件通配符，注意：要包含在引号里，参见 https://github.com/isaacs/node-glob#glob-primer',
-  },
-  outDir: {
-    description: '输出目录',
   },
   engine: {
     type: 'string',
@@ -23,11 +20,10 @@ export const builder: CommandBuilder = {
 
 interface Params {
   sourceGlob: string;
-  outDir: string;
   engine: TranslationEngineType;
 }
 
-export const handler = function ({ sourceGlob, outDir, engine }: Params) {
+export const handler = function ({ sourceGlob, engine }: Params) {
   const kit = new TranslationKit(engine);
   return kit.translateFiles(sourceGlob).subscribe((vfile) => {
     toVFile.writeSync(vfile);
